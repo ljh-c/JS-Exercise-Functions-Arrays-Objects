5a89ecd289f02c0d28a52146ef02743c894e535e
ljh-c <57099181+ljh-c@users.noreply.github.com>
2019-11-11 01:27:28 -0800

    done with challenges 1, 2, 3

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
+function makePersonObject(anId, aName, mail) {
+  return {id: anId, name: aName, email: mail};
 }
 
 /**
@@ -56,8 +56,8 @@ function makePersonObject(/* code here */) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName(personObj) {
+  return `Hello, my name is ${personObj.name}`;
 }
 
 /**
@@ -73,8 +73,14 @@ function getName(/* code here */) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
+function makeSmartPerson(aName) {
+  return {name: aName, 
+          sum: function(num1, num2) {
+            return num1 + num2;
+          },
+          speak: function() {
+            return `Hello, my name is ${this.name}`;
+          }}
 }
 
 
